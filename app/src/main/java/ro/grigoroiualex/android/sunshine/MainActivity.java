ea990b1cd0db4a65a47595e8b7be69b967b9cf96
grigoroiualex <grigoroiualexandru@gmail.com>
2016-04-18 14:30:06 +0300

    Modified the ic_launcher.png image and added a ListView to the main fragment

--- app/src/main/java/ro/grigoroiualex/android/sunshine/MainActivity.java
+++ app/src/main/java/ro/grigoroiualex/android/sunshine/MainActivity.java
@@ -1,7 +1,16 @@
 package ro.grigoroiualex.android.sunshine;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            List<String> weekForecast = new ArrayList<String>();
+            ArrayAdapter<String> adapterForecast =
+                    new ArrayAdapter<String>(
+                            getActivity(),
+                            R.layout.list_item_forecast,
+                            R.id.list_item_forecast_textview,
+                            weekForecast
+                    );
+
+            weekForecast.add("Today - Sunny - 88 / 63");
+            weekForecast.add("Tomorrow - Foggy - 70 / 64");
+            weekForecast.add("Wed - Cloudy - 72 / 63");
+
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(adapterForecast);
+
+            return rootView;
+        }
     }
 }
